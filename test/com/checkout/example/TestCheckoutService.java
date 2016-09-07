7cbf7d08aeef1cb015f6c6b358445f90689646f3
deepaksingla82 <deepaksingla82@gmail.com>
2016-09-07 19:04:16 +0100

    Checkout Service case 2 commit

--- test/com/checkout/example/TestCheckoutService.java
+++ test/com/checkout/example/TestCheckoutService.java
@@ -25,43 +25,71 @@ public class TestCheckoutService extends TestCase {
 	}
 	
 	public void testCheckout_null_List(){
-		double totalPrice = checkoutService.checkout(null);
-		assertEquals(0.0, totalPrice);		
+		float totalPrice = checkoutService.checkout(null);
+		assertEquals(0.0f, totalPrice);		
 	}
 	
 	public void testCheckout_empty_List(){
 		List<String> items = new ArrayList<String>();
-		double totalPrice = checkoutService.checkout(items);
-		assertEquals(0.0, totalPrice);		
+		float totalPrice = checkoutService.checkout(items);
+		assertEquals(0.0f, totalPrice);		
 	}
 	
 	public void testCheckout_apples_only_List(){
-		List<String> items = createApplesList();
-		double totalPrice = checkoutService.checkout(items);
-		assertEquals(2.40, totalPrice);		
+		List<String> items = create5ApplesList();
+		float totalPrice = checkoutService.checkout(items);
+		assertEquals(3.0f, totalPrice);		
 	}
 	
 	public void testCheckout_oranges_only_List(){
-		List<String> items = createOrangesList();
-		double totalPrice = checkoutService.checkout(items);
-		assertEquals(1.00, totalPrice);		
+		List<String> items = create4OrangesList();
+		float totalPrice = checkoutService.checkout(items);
+		assertEquals(1.00f, totalPrice);		
 	}
 	
 	public void testCheckout_apple_oranges_mix_List1(){
-		List<String> items = createAppleOrangesList1();
-		double totalPrice = checkoutService.checkout(items);
-		assertEquals(1.70, totalPrice);		
+		List<String> items = create2Apple2OrangesList1();
+		float totalPrice = checkoutService.checkout(items);
+		assertEquals(1.70f, totalPrice);		
 	}
 	
 	public void testCheckout_apple_oranges_mix_List2(){
-		List<String> items = createAppleOrangesList2();
-		double totalPrice = checkoutService.checkout(items);
-		assertEquals(2.2, totalPrice);		
+		List<String> items = create3Apple5OrangesList2();
+		float totalPrice = checkoutService.checkout(items);
+		assertEquals(3.05f, totalPrice);		
 	}
 	
 	
 	
-	private List<String> createApplesList(){
+	
+	public void testCheckoutwithOffer_apples_only_List(){
+		List<String> items = create5ApplesList();
+		float totalPrice = checkoutService.checkoutWithOffer(items);
+		assertEquals(1.8f, totalPrice);		
+	}
+	
+	public void testCheckoutwithOffer_oranges_only_List(){
+		List<String> items = create4OrangesList();
+		float totalPrice = checkoutService.checkoutWithOffer(items);
+		assertEquals(0.5f, totalPrice);		
+	}
+	
+	public void testCheckoutwithOffer_apple_oranges_mix_List1(){
+		List<String> items = create2Apple2OrangesList1();
+		float totalPrice = checkoutService.checkoutWithOffer(items);
+		assertEquals(1.1f, totalPrice);		
+	}
+	
+	public void testCheckoutwithOffer_apple_oranges_mix_List2(){
+		List<String> items = create3Apple5OrangesList2();
+		float totalPrice = checkoutService.checkoutWithOffer(items);
+		assertEquals(1.95f, totalPrice);		
+	}
+	
+	
+	
+	
+	private List<String> create5ApplesList(){
 		
 		List<String> items = new ArrayList<String>();
 		
@@ -73,16 +101,19 @@ public class TestCheckoutService extends TestCase {
 		
 		String fruit4 = FruitsConstants.APPLE_NAME;
 		
+		String fruit5 = FruitsConstants.APPLE_NAME;
+		
 		items.add(fruit1);
 		items.add(fruit2);
 		items.add(fruit3);
 		items.add(fruit4);
+		items.add(fruit5);
 		
 		return items;
 		
 	}
 	
-	private List<String> createOrangesList(){
+	private List<String> create4OrangesList(){
 		
 		List<String> items = new ArrayList<String>();
 		
@@ -103,7 +134,7 @@ public class TestCheckoutService extends TestCase {
 		
 	}
 	
-	private List<String> createAppleOrangesList1(){
+	private List<String> create2Apple2OrangesList1(){
 		
 		List<String> items = new ArrayList<String>();
 		
@@ -124,7 +155,7 @@ public class TestCheckoutService extends TestCase {
 		
 	}
 	
-	private List<String> createAppleOrangesList2(){
+	private List<String> create3Apple5OrangesList2(){
 		
 		List<String> items = new ArrayList<String>();
 		String fruit1 = FruitsConstants.ORANGE_NAME;
@@ -135,9 +166,13 @@ public class TestCheckoutService extends TestCase {
 		
 		String fruit4 = FruitsConstants.ORANGE_NAME;
 		
-		String fruit5 = FruitsConstants.APPLE_NAME;
+		String fruit5 = FruitsConstants.ORANGE_NAME;
 		
 		String fruit6 = FruitsConstants.APPLE_NAME;
+		
+		String fruit7 = FruitsConstants.APPLE_NAME;
+		
+		String fruit8 = FruitsConstants.APPLE_NAME;
 		
 		items.add(fruit1);
 		items.add(fruit2);
@@ -145,6 +180,8 @@ public class TestCheckoutService extends TestCase {
 		items.add(fruit4);
 		items.add(fruit5);
 		items.add(fruit6);
+		items.add(fruit7);
+		items.add(fruit8);
 		
 		return items;
 		
